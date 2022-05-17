2134e5a3f258b89f475becd5755c00abb9fea27f
PT-ATA No One <ata-no-one@gdata.de>
2022-05-17 13:07:35 +0000

    Adding black

--- python/src/vaas_test.py
+++ python/src/vaas_test.py
@@ -8,7 +8,7 @@ from vaas import Vaas
 
 load_dotenv()
 
-TOKEN = os.getenv('VAAS_TOKEN')
+TOKEN = os.getenv("VAAS_TOKEN")
 EICAR_BASE64 = "WDVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCo="
 
 
@@ -26,13 +26,17 @@ class VaasTest(unittest.IsolatedAsyncioTestCase):
     async def test_for_sha256_returns_clean_for_clean_sha256(self):
         async with Vaas() as vaas:
             await vaas.connect(TOKEN)
-            verdict = await vaas.for_sha256("698CDA840A0B3D4639F0C5DBD5C629A847A27448A9A179CB6B7A648BC1186F23")
+            verdict = await vaas.for_sha256(
+                "698CDA840A0B3D4639F0C5DBD5C629A847A27448A9A179CB6B7A648BC1186F23"
+            )
             self.assertEqual(verdict, "Clean")
 
     async def test_for_sha256_returns_malicious_for_eicar(self):
         async with Vaas() as vaas:
             await vaas.connect(TOKEN)
-            verdict = await vaas.for_sha256("275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f")
+            verdict = await vaas.for_sha256(
+                "275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f"
+            )
             self.assertEqual(verdict, "Malicious")
 
     async def test_for_buffer_returns_malicious_for_eicar(self):
@@ -58,5 +62,5 @@ class VaasTest(unittest.IsolatedAsyncioTestCase):
             self.assertEqual(verdict, "Malicious")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
