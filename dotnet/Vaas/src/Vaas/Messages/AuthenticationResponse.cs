f65075a9896449f693f40941b828a3b5c5258ce2
PT-ATA No One <ata-no-one@gdata.de>
2022-05-25 15:22:53 +0200

    Use file scoped namespaces

--- dotnet/Vaas/src/Vaas/Messages/AuthenticationResponse.cs
+++ dotnet/Vaas/src/Vaas/Messages/AuthenticationResponse.cs
@@ -1,24 +1,23 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
 
-namespace Vaas.Messages
+namespace Vaas.Messages;
+
+public class AuthenticationResponse
 {
-    public class AuthenticationResponse
-    {
-        [JsonPropertyName("kind")] 
-        public string Kind = "AuthResponse";
+    [JsonPropertyName("kind")] 
+    public string Kind = "AuthResponse";
         
-        [JsonPropertyName("success")] 
-        public bool Success { get; init; }
+    [JsonPropertyName("success")] 
+    public bool Success { get; init; }
 
-        [JsonPropertyName("session_id")] 
-        public string? SessionId { get; init; }
+    [JsonPropertyName("session_id")] 
+    public string? SessionId { get; init; }
 
-        [JsonPropertyName("text")] 
-        public string? Text { get; init; }
+    [JsonPropertyName("text")] 
+    public string? Text { get; init; }
 
-        [MemberNotNullWhen(true, nameof(SessionId), nameof(Text))]
-        public bool IsValid => !string.IsNullOrWhiteSpace(SessionId)
-                               && !string.IsNullOrWhiteSpace(Text);
-    }
+    [MemberNotNullWhen(true, nameof(SessionId), nameof(Text))]
+    public bool IsValid => !string.IsNullOrWhiteSpace(SessionId)
+                           && !string.IsNullOrWhiteSpace(Text);
 }
