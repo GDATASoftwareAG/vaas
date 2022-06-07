6e2119f49630720500dfe16f0506228d7a63e19f
Philip Stadermann <philip.stadermann@gdata.de>
2022-06-07 11:30:59 +0200

    Format with prettier

--- typescript/src/messages/verdict_response.ts
+++ typescript/src/messages/verdict_response.ts
@@ -1,18 +1,16 @@
-import {Serializable, JsonProperty} from 'typescript-json-serializer';
-import {Kind, Message} from "./message";
-import {Verdict} from "../verdict";
-
+import { Serializable, JsonProperty } from "typescript-json-serializer";
+import { Kind, Message } from "./message";
+import { Verdict } from "../verdict";
 
 @Serializable()
 export class VerdictResponse extends Message {
-
-    public constructor(
-        @JsonProperty() public sha256: string,
-        @JsonProperty() public guid: string,
-        @JsonProperty() public verdict: Verdict,
-        @JsonProperty() public upload_token: string | undefined,
-        @JsonProperty() public url: string | undefined,
-    ) {
-        super(Kind.VerdictResponse)
-    }
+  public constructor(
+    @JsonProperty() public sha256: string,
+    @JsonProperty() public guid: string,
+    @JsonProperty() public verdict: Verdict,
+    @JsonProperty() public upload_token: string | undefined,
+    @JsonProperty() public url: string | undefined
+  ) {
+    super(Kind.VerdictResponse);
+  }
 }
