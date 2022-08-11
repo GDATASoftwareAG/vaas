9cb4a2ae8c9308d953c88d67531402e0571208ac
unglaublicherdude <github@matthias-simonis.de>
2022-08-11 12:17:59 +0200

    adjust tests to be run from a different directory

--- php/tests/vaas/VaasTest.php
+++ php/tests/vaas/VaasTest.php
@@ -2,7 +2,7 @@
 
 namespace VaasTesting;
 
-require_once "./vendor/autoload.php";
+require_once __DIR__ . "/vendor/autoload.php";
 
 use PHPUnit\Framework\TestCase;
 use Prophecy\PhpUnit\ProphecyTrait;
