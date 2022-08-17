cd0f740c9eaf4f5e2623b3fcef987b585b0c58e8
PT-ATA No One <ata-no-one@gdata.de>
2022-08-17 14:43:45 +0200

    add manual test for testing with big files

---
+++ dotnet/Vaas/test/TestFiles/Program.cs
@@ -0,0 +1,31 @@
+﻿if (args.Length == 0)
+{
+    Console.WriteLine("Usage TestFiles [FILE] ");
+    Environment.Exit(1);
+}
+
+var vaas = await AuthenticateWithCredentials();
+Console.WriteLine("Connected to VaaS");
+
+foreach (var path in args)
+{
+    Console.WriteLine($"Testing {path}");
+    var verdict = await vaas.ForFileAsync(path);
+    Console.WriteLine($"Tested {path}: Verdict {verdict}");
+}
+
+static async Task<Vaas.Vaas> AuthenticateWithCredentials()
+{
+    DotNetEnv.Env.TraversePath().Load();
+    var url = DotNetEnv.Env.GetString(
+        "VAAS_URL",
+        "wss://gateway-vaas.gdatasecurity.de");
+    var tokenEndpoint = new Uri(DotNetEnv.Env.GetString(
+        "TOKEN_URL",
+        "https://keycloak-vaas.gdatasecurity.de/realms/vaas/protocol/openid-connect/token"));
+    var clientId = DotNetEnv.Env.GetString("CLIENT_ID");
+    var clientSecret = DotNetEnv.Env.GetString("CLIENT_SECRET");
+    var vaas = new Vaas.Vaas();
+    await vaas.ConnectWithCredentials(clientId, clientSecret, tokenEndpoint, url);
+    return vaas;
+}
