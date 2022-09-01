ad8365bd5979b6753e481e7d74b93dc25a446f09
PT-ATA No One <ata-no-one@gdata.de>
2022-09-01 12:52:49 +0000

    test-files.ts example

---
+++ typescript/tests/createVaas.ts
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import Vaas from "../src/vaas";
+import createVaasWithClientCredentialsGrant from "../src/createVaasWithClientCredentialsGrant";
+
+function throwError(errorMessage: string): never {
+  throw new Error(errorMessage);
+}
+
+function getFromEnvironment(key: string) {
+  return (
+    process.env[key] ?? throwError(`Set ${key} in environment or .env file`)
+  );
+}
+
+export default function createVaas(): Promise<Vaas> {
+  dotenv.config();
+  const CLIENT_ID = getFromEnvironment("CLIENT_ID");
+  const CLIENT_SECRET = getFromEnvironment("CLIENT_SECRET");
+  const VAAS_URL = getFromEnvironment("VAAS_URL");
+  const TOKEN_URL = getFromEnvironment("TOKEN_URL");
+
+  return createVaasWithClientCredentialsGrant(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    TOKEN_URL,
+    VAAS_URL
+  );
+}
