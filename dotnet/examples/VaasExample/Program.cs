7df3d793dca8932860670d18b79a46fb205850ad
Kevin Heise <kevin.heise@gdata.de>
2022-09-15 10:52:34 +0200

    Create minimal example for running Vaas on dotnet

---
+++ dotnet/examples/VaasExample/Program.cs
@@ -0,0 +1,7 @@
+﻿var vaas = new Vaas.Vaas();
+await vaas.ConnectWithCredentials("clientId", "clientSecret", new Uri("https://keycloak-vaas.gdatasecurity.de/realms/vaas/protocol/openid-connect/token"));
+
+const string file = "/path/to/file";
+var verdict = await vaas.ForFileAsync(file);
+
+Console.WriteLine($"File {file} is {verdict}", file, verdict);
