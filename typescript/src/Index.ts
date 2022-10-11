5effe773daf13f04afbe6f9dc2478a74d8ae0d23
PT-ATA No One <ata-no-one@gdata.de>
2022-10-11 14:09:24 +0000

    export vaas errors in index

--- typescript/src/Index.ts
+++ typescript/src/Index.ts
@@ -2,4 +2,5 @@ export * from "./Vaas";
 export * from "./CreateVaasWithClientCredentialsGrant";
 export * from "./Verdict";
 export * from "./CancellationToken";
+export * from "./VaasErrors";
 export { default as ClientCredentialsGrantAuthenticator } from "./ClientCredentialsGrantAuthenticator";
