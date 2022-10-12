cf15d42b383da07408abcdbcd567ce95c08b53cc
PT-ATA No One <ata-no-one@gdata.de>
2022-10-12 14:36:51 +0200

    Build on path condition; simplify example

--- typescript/examples/VaasExample/main.ts
+++ typescript/examples/VaasExample/main.ts
@@ -20,38 +20,24 @@ async function main() {
   const CLIENT_ID = getFromEnvironment("CLIENT_ID");
   const CLIENT_SECRET = getFromEnvironment("CLIENT_SECRET");
   const SCAN_PATH = getFromEnvironment("SCAN_PATH");
+  const TOKEN_URL =
+    "https://keycloak-vaas.gdatasecurity.de/realms/vaas/protocol/openid-connect/token";
 
   const authenticator = new ClientCredentialsGrantAuthenticator(
     CLIENT_ID,
     CLIENT_SECRET,
-    "https://keycloak-vaas.gdatasecurity.de/realms/vaas/protocol/openid-connect/token"
+    TOKEN_URL
   );
 
   const vaas = new Vaas();
+  await vaas.connect(await authenticator.getToken());
 
   const f = await fs.open(SCAN_PATH, "r");
 
-  try {
-    while (true) {
-      try {
-        const verdict = await vaas.forFile(await f.readFile());
-        console.log(verdict);
-        break;
-      } catch (e) {
-        if (e instanceof VaasConnectionClosedError) {
-          await vaas.connect(await authenticator.getToken());
-          continue;
-        }
-        if (e instanceof VaasTimeoutError) {
-          continue;
-        }
-        throw e;
-      }
-    }
-  } finally {
-    f.close();
-    vaas.close();
-  }
+  const verdict = await vaas.forFile(await f.readFile());
+  console.log(verdict);
+  f.close();
+  vaas.close();
 }
 
 main().catch((e) => {
