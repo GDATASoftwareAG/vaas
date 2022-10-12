4dac5f05d05894560794dfc8b689fa3b715f6996
borisahrens <45789221+borisahrens@users.noreply.github.com>
2022-10-12 14:47:32 +0200

    fix example (#111)
    * fix example
    
    * Build on path condition; simplify example
    
    * Upgrade action setups
    
    Co-authored-by: PT-ATA No One <ata-no-one@gdata.de>

--- typescript/examples/VaasExample/main.ts
+++ typescript/examples/VaasExample/main.ts
@@ -1,5 +1,10 @@
 import { promises as fs } from "fs";
-import { CreateVaasWithClientCredentialsGrant } from "gdata-vaas";
+import {
+  ClientCredentialsGrantAuthenticator,
+  Vaas,
+  VaasConnectionClosedError,
+  VaasTimeoutError,
+} from "gdata-vaas";
 
 function throwError(errorMessage: string): never {
   throw new Error(errorMessage);
@@ -15,20 +20,24 @@ async function main() {
   const CLIENT_ID = getFromEnvironment("CLIENT_ID");
   const CLIENT_SECRET = getFromEnvironment("CLIENT_SECRET");
   const SCAN_PATH = getFromEnvironment("SCAN_PATH");
+  const TOKEN_URL =
+    "https://keycloak-vaas.gdatasecurity.de/realms/vaas/protocol/openid-connect/token";
 
-  const vaas = await CreateVaasWithClientCredentialsGrant(
+  const authenticator = new ClientCredentialsGrantAuthenticator(
     CLIENT_ID,
     CLIENT_SECRET,
-    "https://keycloak-vaas.gdatasecurity.de/realms/vaas/protocol/openid-connect/token"
+    TOKEN_URL
   );
+
+  const vaas = new Vaas();
+  await vaas.connect(await authenticator.getToken());
+
   const f = await fs.open(SCAN_PATH, "r");
-  try {
-    const verdict = await vaas.forFile(await f.readFile());
-    console.log(verdict);
-  } finally {
-    f.close();
-    vaas.close();
-  }
+
+  const verdict = await vaas.forFile(await f.readFile());
+  console.log(verdict);
+  f.close();
+  vaas.close();
 }
 
 main().catch((e) => {
