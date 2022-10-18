ec00bb4ade4442d8dcd2a4f17b58a2dba2f2b570
PT-ATA No One <ata-no-one@gdata.de>
2022-10-18 12:34:56 +0000

    adjust example to new reconnect workflow

--- python/examples/VaasExample/main.py
+++ python/examples/VaasExample/main.py
@@ -1,15 +1,17 @@
+from argparse import _AttributeHolder
 import asyncio
 import os
-from vaas import Vaas
+from vaas import Vaas, ClientCredentialsGrantAuthenticator
 
 
 async def main():
+    authenticator = ClientCredentialsGrantAuthenticator(
+        os.getenv("CLIENT_ID"),
+        os.getenv("CLIENT_SECRET"),
+        "https://keycloak-vaas.gdatasecurity.de/realms/vaas/protocol/openid-connect/token"
+    )
     async with Vaas() as vaas:
-        await vaas.connect_with_client_credentials(
-            os.getenv("CLIENT_ID"),
-            os.getenv("CLIENT_SECRET"),
-            "https://keycloak-vaas.gdatasecurity.de/realms/vaas/protocol/openid-connect/token",
-        )
+        await vaas.connect(await authenticator.get_token())
         path = os.getenv("SCAN_PATH")
         verdict = await vaas.for_file(path)
         print(f"File {path} is detected as {verdict}")
