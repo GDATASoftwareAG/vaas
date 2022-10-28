569ff5b9684ba6fdf0457d6afe2d2a90b0367fec
doxthree <kevin.heise@tutanota.de>
2022-10-28 13:59:06 +0200

    fix discord & vaas example (#125)
    Co-authored-by: Kevin Heise <kevin.heise@gdata.de>

--- typescript/examples/DiscordBot/src/index.ts
+++ typescript/examples/DiscordBot/src/index.ts
@@ -1,14 +1,34 @@
 import { Client } from "discord.js";
 import axios from "axios";
 import * as dotenv from "dotenv";
-import Vaas from "../../../src/vaas";
+import {
+    ClientCredentialsGrantAuthenticator,
+    Vaas,
+  } from "gdata-vaas";
 
-const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] });
+  function throwError(errorMessage: string): never {
+    throw new Error(errorMessage);
+  }
+  
+  function getFromEnvironment(key: string) {
+    return (
+      process.env[key] ?? throwError(`Set ${key} in environment or .env file`)
+    );
+  }
+  
+const client = new Client({ intents: ["Guilds", "GuildMessages", "DirectMessages", "MessageContent"]});
 dotenv.config()
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user!.username}`);
 })
+
+const DISCORD_TOKEN = getFromEnvironment("DISCORD_TOKEN")
+const CLIENT_ID = getFromEnvironment("CLIENT_ID");
+const CLIENT_SECRET = getFromEnvironment("CLIENT_SECRET");
+const TOKEN_URL =
+  "https://keycloak-vaas.gdatasecurity.de/realms/vaas/protocol/openid-connect/token";
+
 
 client.on("messageCreate", (msg) => {
     const attachments = Array.from(msg.attachments.values());
@@ -19,10 +39,18 @@ client.on("messageCreate", (msg) => {
             method: 'GET',
             responseType: "arraybuffer" // important
         }).then(async (response) => {
+            console.log(`Scan File ${attachment.name}`)
+            const authenticator = new ClientCredentialsGrantAuthenticator(
+                CLIENT_ID,
+                CLIENT_SECRET,
+                TOKEN_URL
+              );
+            
             const vaas = new Vaas();
-            const connection = await vaas.connect(process.env.VAAS_TOKEN!);
-            const verdict = await vaas.forFile(connection, response.data);
-            if (verdict === "Malicious") {
+            const token = await authenticator.getToken()
+            await vaas.connect(token);
+            const verdict = await vaas.forFile(response.data);
+            if (verdict.verdict === "Malicious") {
                 msg.delete()
                     .catch(console.error);
                 msg.channel.send(`${attachment.name} was deleted, because I detected it as malicious.`)
@@ -31,4 +59,4 @@ client.on("messageCreate", (msg) => {
     })
 })
 
-client.login(process.env.DISCORD_TOKEN);
+client.login(DISCORD_TOKEN);
