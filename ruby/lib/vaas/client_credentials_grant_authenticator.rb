daca8e7951ee9b744a57dc9e001974ee2fb9d761
Allie Weitenkamp <allie.weitenkamp@gdata.de>
2023-01-11 13:04:25 +0100

    cleanup and adjust error handling

--- ruby/lib/vaas/client_credentials_grant_authenticator.rb
+++ ruby/lib/vaas/client_credentials_grant_authenticator.rb
@@ -3,31 +3,31 @@ require 'async'
 require 'async/http/internet'
 
 module VAAS
-class ClientCredentialsGrantAuthenticator
+  class ClientCredentialsGrantAuthenticator
 
     attr_accessor :client_id, :client_secret, :token_endpoint, :token
 
     def initialize(client_id, client_secret, token_endpoint)
-        @client_id = client_id
-        @client_secret = client_secret
-        @token_endpoint = token_endpoint
+      @client_id = client_id
+      @client_secret = client_secret
+      @token_endpoint = token_endpoint
     end
 
     def get_token
-        Async do
-            client = Async::HTTP::Internet.new
+      Async do
+        client = Async::HTTP::Internet.new
 
-            header = [['content-type', 'application/x-www-form-urlencoded']]
-            body = ["grant_type=client_credentials&client_id=#{client_id}&client_secret=#{client_secret}"]
+        header = [['content-type', 'application/x-www-form-urlencoded']]
+        body = ["grant_type=client_credentials&client_id=#{client_id}&client_secret=#{client_secret}"]
 
-            response = client.post(token_endpoint, header, body)
-            self.token = JSON.parse(response.read)['access_token']
-        rescue => e
-            p e
-        ensure
-            client&.close
-        end
-        token
+        response = client.post(token_endpoint, header, body)
+        self.token = JSON.parse(response.read)['access_token']
+      rescue
+        raise VaasAuthenticationError
+      ensure
+        client&.close
+      end
+      token
     end
-end
+  end
 end
