39234f0936850a81ce3e3566c1b481c34918a537
Spiridon Fotiadis <s.fot@live.com>
2023-01-19 23:10:50 +0100

    refactoring

--- go/go.mod
+++
@@ -1,5 +0,0 @@
-module vaas
-
-go 1.17
-
-require github.com/gorilla/websocket v1.5.0 // indirect
--- go/src/messages/authenticationRequest.go
+++
@@ -1,9 +0,0 @@
-package messages
-
-type AuthRequest struct {
-	Kind      string `json:"kind"`
-	Token     string `json:"token"`
-	SessionId string `json:"session_id"`
-}
-
-
