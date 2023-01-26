23a3a9c9e6a6b2759c74879f3918176338c3e84a
Allie Weitenkamp <allie.weitenkamp@gdata.de>
2023-01-26 18:33:47 +0100

    add timeout

--- ruby/lib/vaas/vaas_main.rb
+++ ruby/lib/vaas/vaas_main.rb
@@ -11,6 +11,8 @@ require 'uri'
 require_relative 'vaas_verdict'
 require_relative 'vaas_errors'
 
+TIMEOUT = 600
+
 module VAAS
   class VaasMain
 
@@ -76,79 +78,95 @@ module VAAS
     end
 
     def for_sha256(sha256)
-      Async do
-        verdict_notification = Async::Notification.new
-        guid = SecureRandom.uuid.to_s
-        websocket = get_authenticated_websocket
-        verdict_request =  JSON.generate({:kind => "VerdictRequest",
-                                          :session_id => @session_id,
-                                          :sha256 => sha256,
-                                          :guid => guid})
-        @requests[guid] = verdict_notification
-        websocket.write(verdict_request)
-        websocket.flush
-        VaasVerdict.new(verdict_notification.wait)
+      Async do |task|
+        task.with_timeout(TIMEOUT) do
+          verdict_notification = Async::Notification.new
+          guid = SecureRandom.uuid.to_s
+          websocket = get_authenticated_websocket
+          verdict_request =  JSON.generate({:kind => "VerdictRequest",
+                                            :session_id => @session_id,
+                                            :sha256 => sha256,
+                                            :guid => guid})
+          @requests[guid] = verdict_notification
+          websocket.write(verdict_request)
+          websocket.flush
+          VaasVerdict.new(verdict_notification.wait)
+        rescue Async::TimeoutError
+          raise VaasTimeoutError
+        end
       end
     end
 
     def for_url(url)
-      Async do
-        verdict_notification = Async::Notification.new
-        guid = SecureRandom.uuid.to_s
-        websocket = get_authenticated_websocket
-        verdict_request =  JSON.generate({:kind => "VerdictRequestForUrl",
-                                          :session_id => @session_id,
-                                          :url => url,
-                                          :guid => guid})
-        @requests[guid] = verdict_notification
-        websocket.write(verdict_request)
-        websocket.flush
-        VaasVerdict.new(verdict_notification.wait)
+      Async do |task|
+        task.with_timeout(TIMEOUT) do
+          verdict_notification = Async::Notification.new
+          guid = SecureRandom.uuid.to_s
+          websocket = get_authenticated_websocket
+          verdict_request =  JSON.generate({:kind => "VerdictRequestForUrl",
+                                            :session_id => @session_id,
+                                            :url => url,
+                                            :guid => guid})
+          @requests[guid] = verdict_notification
+          websocket.write(verdict_request)
+          websocket.flush
+          VaasVerdict.new(verdict_notification.wait)
+        rescue Async::TimeoutError
+          raise VaasTimeoutError
+        end
       end
     end
 
     def for_file(path)
-      Async do
-        sha256 = Digest::SHA256.file(path).hexdigest
-        verdict_notification = Async::Notification.new
-        guid = SecureRandom.uuid.to_s
-        websocket = get_authenticated_websocket
-        verdict_request =  JSON.generate({:kind => "VerdictRequest",
-                                          :session_id => @session_id,
-                                          :sha256 => sha256,
-                                          :guid => guid})
-        @requests[guid] = verdict_notification
-        websocket.write(verdict_request)
-        websocket.flush
-        message = verdict_notification.wait
+      Async do |task|
+        task.with_timeout(TIMEOUT) do
+          sha256 = Digest::SHA256.file(path).hexdigest
+          verdict_notification = Async::Notification.new
+          guid = SecureRandom.uuid.to_s
+          websocket = get_authenticated_websocket
+          verdict_request =  JSON.generate({:kind => "VerdictRequest",
+                                            :session_id => @session_id,
+                                            :sha256 => sha256,
+                                            :guid => guid})
+          @requests[guid] = verdict_notification
+          websocket.write(verdict_request)
+          websocket.flush
+          message = verdict_notification.wait
 
-        if message['verdict'] == "Unknown"
-          upload_notification = Async::Notification.new
-          @requests[guid] = upload_notification
-          upload(message, path)
-          VaasVerdict.new(upload_notification.wait)
-        else
-          VaasVerdict.new(message)
+          if message['verdict'] == "Unknown"
+            upload_notification = Async::Notification.new
+            @requests[guid] = upload_notification
+            upload(message, path)
+            VaasVerdict.new(upload_notification.wait)
+          else
+            VaasVerdict.new(message)
+          end
+        rescue Async::TimeoutError
+          raise VaasTimeoutError
         end
       end
     end
 
     def upload(message, path)
-      Async do
-        token = message['upload_token']
-        url = message['url']
+      Async do |task|
+        task.with_timeout(TIMEOUT) do
+          token = message['upload_token']
+          url = message['url']
 
-        client = Async::HTTP::Internet.new
+          client = Async::HTTP::Internet.new
 
-        header = [['authorization', token]]
-        body = Protocol::HTTP::Body::File.open(File.join(path))
+          header = [['authorization', token]]
+          body = Protocol::HTTP::Body::File.open(File.join(path))
 
-        response = client.put(url, header, body)
-        response.read
+          response = client.put(url, header, body)
+          response.read
 
-        raise VaasUploadError, "Upload failed with code: #{response.status}" if response.status != 200
-      ensure
-        client&.close
+          raise VaasUploadError, "Upload failed with code: #{response.status}" if response.status != 200
+        rescue Async::TimeoutError
+          raise VaasTimeoutError, "Upload timed out"
+        ensure
+          client&.close
+        end
       end
     end
   end
