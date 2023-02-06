dcbef45680cfff26bb2f09654df9d6b148084d84
Spiridon Fotiadis <s.fot@live.com>
2023-02-06 15:43:49 +0100

    adds constructor function for VerdictRequest

--- go/vaas/pkg/messages/verdict_request.go
+++ go/vaas/pkg/messages/verdict_request.go
@@ -1,5 +1,10 @@
 package messages
 
+import (
+	"github.com/google/uuid"
+	"vaas/pkg/options"
+)
+
 type VerdictRequest struct {
 	Kind      string `json:"kind" default:"VerdictRequest"`
 	Sha256    string `json:"sha256"`
@@ -7,4 +12,15 @@ type VerdictRequest struct {
 	SessionID string `json:"session_id"`
 	UseCache  bool   `json:"use_cache"`
 	UseShed   bool   `json:"use_shed"`
+}
+
+func NewVerdictRequest(sessionId string, options options.VaasOptions, sha256 string) VerdictRequest {
+	return VerdictRequest{
+		Kind:      "VerdictRequest",
+		Sha256:    sha256,
+		SessionID: sessionId,
+		Guid:      uuid.New().String(),
+		UseCache:  options.UseCache,
+		UseShed:   options.UseShed,
+	}
 }
