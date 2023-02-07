b3670888810107b36ee58f65a296f9182252a5e6
Spiridon Fotiadis <s.fot@live.com>
2023-02-07 15:29:23 +0100

    refactoring test utilities

--- go/vaas/test/authenticator/authenticator_test.go
+++ go/vaas/test/authenticator/authenticator_test.go
@@ -2,10 +2,11 @@ package authenticator_test
 
 import (
 	"log"
-	"os"
 	"testing"
 
 	"vaas/pkg/authenticator"
+	utilities "vaas/test/test_utilities"
+
 
 	"github.com/joho/godotenv"
 )
@@ -15,7 +16,7 @@ func TestGetToken_WithValidCredentials_GotToken(t *testing.T) {
 		log.Println(err)
 	}
 
-	CLIENT_ID, CLIENT_SECRET, TOKEN_ENDPOINT := readCredentials()
+	CLIENT_ID, CLIENT_SECRET, TOKEN_ENDPOINT := utilities.ReadCredentials()
 
 	authenticator := authenticator.New(CLIENT_ID,CLIENT_SECRET, TOKEN_ENDPOINT)
 	var accessToken string
@@ -30,7 +31,7 @@ func TestGetToken_WithWrongCredentials_Error(t *testing.T) {
 		log.Fatal(err)
 	}
 
-	_, _, TOKEN_ENDPOINT := readCredentials()
+	_, _, TOKEN_ENDPOINT := utilities.ReadCredentials()
 
 	authenticator := authenticator.New("foo", "bar", TOKEN_ENDPOINT)
 	var accessToken string
@@ -38,21 +39,4 @@ func TestGetToken_WithWrongCredentials_Error(t *testing.T) {
 	if err == nil{
 		t.Fatalf(`GetToken(&accessToken) = %q, expected "accesstoken is null"`, err)
 	}
-}
-
-
-func readCredentials() (string, string, string) {
-	CLIENT_ID, exists := os.LookupEnv("CLIENT_ID")
-	if !exists {
-		log.Fatal("no Client ID set")
-	}
-	CLIENT_SECRET, exists := os.LookupEnv("CLIENT_SECRET")
-	if !exists {
-		log.Fatal("no Client Secret set")
-	}
-	TOKEN_ENDPOINT, exists := os.LookupEnv("TOKEN_ENDPOINT")
-	if !exists {
-		log.Fatal("no token endpoint configured")
-	}
-	return CLIENT_ID, CLIENT_SECRET, TOKEN_ENDPOINT
 }
---
+++ go/vaas/test/test_utilities/test_utilities.go
@@ -0,0 +1,22 @@
+package test_utilities
+
+import (
+	"os"
+	"log"
+)
+
+func ReadCredentials() (string, string, string) {
+	CLIENT_ID, exists := os.LookupEnv("CLIENT_ID")
+	if !exists {
+		log.Fatal("no Client ID set")
+	}
+	CLIENT_SECRET, exists := os.LookupEnv("CLIENT_SECRET")
+	if !exists {
+		log.Fatal("no Client Secret set")
+	}
+	TOKEN_ENDPOINT, exists := os.LookupEnv("TOKEN_ENDPOINT")
+	if !exists {
+		log.Fatal("no token endpoint configured")
+	}
+	return CLIENT_ID, CLIENT_SECRET, TOKEN_ENDPOINT
+}
