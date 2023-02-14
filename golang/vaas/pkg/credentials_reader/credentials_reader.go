e74035020e6c1857429ed6a6d005f3b816f5465e
Spiridon Fotiadis <s.fot@live.com>
2023-02-14 13:49:52 +0100

    adds credentialsreader package

---
+++ golang/vaas/pkg/credentials_reader/credentials_reader.go
@@ -0,0 +1,27 @@
+package credentialsreader
+
+import (
+	"os"
+	"log"	
+)
+
+func ReadCredentials() (CLIENT_ID string, CLIENT_SECRET string, VAAS_URL string, TOKEN_ENDPOINT string) {
+	ci, exists := os.LookupEnv("CLIENT_ID")
+	if !exists {
+		log.Fatal("no Client ID set")
+	}
+	cs, exists := os.LookupEnv("CLIENT_SECRET")
+	if !exists {
+		log.Fatal("no Client Secret set")
+	}
+	vu, exists := os.LookupEnv("VAAS_URL")
+	if !exists {
+		log.Fatal("no token endpoint configured")
+	}
+	te, exists := os.LookupEnv("TOKEN_ENDPOINT")
+	if !exists {
+		log.Fatal("no token endpoint configured")
+	}
+	
+	return ci, cs, vu, te
+}
