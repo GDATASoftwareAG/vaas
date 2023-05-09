475988d26423ccfa0f46ce8a01accc2829b6cd6e
PT-ATA No One <ata-no-one@gdata.de>
2023-05-09 15:45:46 +0200

    fix php examples

--- php/examples/VaasExample/GetVerdictByFile.php
+++ php/examples/VaasExample/GetVerdictByFile.php
@@ -2,6 +2,7 @@
 
 namespace VaasExamples;
 
+use Monolog\Logger;
 use VaasSdk\ClientCredentialsGrantAuthenticator;
 use VaasSdk\Vaas;
 
@@ -12,9 +13,11 @@ $authenticator = new ClientCredentialsGrantAuthenticator(
     getenv("CLIENT_SECRET"),
     "https://account.gdata.de/realms/vaas-production/protocol/openid-connect/token"
 );
+
 $vaas = new Vaas(
     "wss://gateway.production.vaas.gdatasecurity.de"
 );
+
 $vaas->Connect($authenticator->getToken());
 $scanPath = getenv("SCAN_PATH");
 $vaasVerdict = $vaas->ForFile($scanPath);
