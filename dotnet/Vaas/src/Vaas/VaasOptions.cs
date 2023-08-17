b69582c257a0c7a989cd434ef50cce29d9785bda
PT-ATA No One <ata-no-one@gdata.de>
2023-08-17 16:30:33 +0200

    Improve error handling for DotNet SDK #282 Review fixes

--- dotnet/Vaas/src/Vaas/VaasOptions.cs
+++ dotnet/Vaas/src/Vaas/VaasOptions.cs
@@ -2,8 +2,8 @@ namespace Vaas;
 
 public class VaasOptions
 {
-    public static bool? UseHashLookup => null;
-    public static bool? UseCache => null;
+    public bool? UseHashLookup { get; init; } = null;
+    public bool? UseCache { get; init; } =null;
 
     public static readonly VaasOptions Defaults = new();
 }
