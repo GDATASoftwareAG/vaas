d67a378b15055462355534e63ee94926186b5ca0
Philip Stadermann <philip.stadermann@gdata.de>
2023-08-25 08:37:40 +0200

    Include ResourceOwnerPasswordGrantAuthenticator for SDK #289
    Example

--- dotnet/examples/VaasExample/Program.cs
+++ dotnet/examples/VaasExample/Program.cs
@@ -1,11 +1,12 @@
-using Vaas;
-
 namespace VaasExample;
 
 public static class Program
 {
     private static string ClientId => Environment.GetEnvironmentVariable("CLIENT_ID") ?? string.Empty;
+    private static string ClientIdForResourceOwnerPasswordGrant => Environment.GetEnvironmentVariable("VAAS_CLIENT_ID") ?? string.Empty;
     private static string ClientSecret => Environment.GetEnvironmentVariable("CLIENT_SECRET") ?? string.Empty;
+    private static string UserName => Environment.GetEnvironmentVariable("USER_NAME") ?? string.Empty;
+    private static string Password => Environment.GetEnvironmentVariable("PASSWORD") ?? string.Empty;
     private static Uri VaasUrl => new Uri(Environment.GetEnvironmentVariable("VAAS_URL") ??
                                           "wss://gateway.production.vaas.gdatasecurity.de");
     private static Uri TokenUrl => new Uri(Environment.GetEnvironmentVariable("TOKEN_URL") ??
@@ -34,11 +35,14 @@ public static class Program
         {
             Url = VaasUrl,
         };
-        var authenticator = new ClientCredentialsGrantAuthenticator(
-            ClientId,
-            ClientSecret,
-            TokenUrl
-        );
+        // If you got username + password
+        var authenticator = new ResourceOwnerPasswordGrantAuthenticator(ClientIdForResourceOwnerPasswordGrant, UserName, Password, TokenUrl);
+        // else if you got client id and client secret
+        // var authenticator = new ClientCredentialsGrantAuthenticator(
+        //     ClientId,
+        //     ClientSecret,
+        //     TokenUrl
+        // );
         await vaas.Connect(await authenticator.GetToken());
         return vaas;
     }
