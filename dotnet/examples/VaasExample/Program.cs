31d17515c426a7b1d939a25d25074c591e0562a3
PT-ATA No One <ata-no-one@gdata.de>
2023-11-06 11:19:50 +0100

    update vaas sdk version for dotnet example

--- dotnet/examples/VaasExample/Program.cs
+++ dotnet/examples/VaasExample/Program.cs
@@ -1,11 +1,12 @@
 using Vaas;
+using Vaas.Authentication;
 
 namespace VaasExample;
 
 public static class Program
 {
     private static string ClientId => Environment.GetEnvironmentVariable("CLIENT_ID") ?? string.Empty;
-    private static string ClientIdForResourceOwnerPasswordGrant => Environment.GetEnvironmentVariable("VAAS_CLIENT_ID") ?? string.Empty;
+    private static string ClientIdForResourceOwnerPasswordGrant => Environment.GetEnvironmentVariable("VAAS_CLIENT_ID") ?? "vaas-customer";
     private static string ClientSecret => Environment.GetEnvironmentVariable("CLIENT_SECRET") ?? string.Empty;
     private static string UserName => Environment.GetEnvironmentVariable("VAAS_USER_NAME") ?? string.Empty;
     private static string Password => Environment.GetEnvironmentVariable("VAAS_PASSWORD") ?? string.Empty;
@@ -26,38 +27,54 @@ public static class Program
     {
         var vaas = await CreateVaasAndConnect();
         var file = Environment.GetEnvironmentVariable("SCAN_PATH") ?? string.Empty;
-        var verdict = await vaas.ForFileAsync(file);
+        var verdict = await vaas.ForFileAsync(file, CancellationToken.None);
 
         Console.WriteLine($"{verdict.Sha256} is detected as {verdict.Verdict}");
     }
 
-    private static async Task<Vaas.Vaas> CreateVaasAndConnect()
+    private static async Task<IVaas> CreateVaasAndConnect()
     {
-        var vaas = new Vaas.Vaas()
+        
+        // If you got a username and password from us, you can use the GrantType.Password like this
+        // You may use self registration and create a new username and password for the
+        // Credentials by yourself like the example above on https://vaas.gdata.de/login
+        var vaas = VaasFactory.Create(new VaasOptions()
         {
             Url = VaasUrl,
-        };
-        // If you got a username and password from us, you can use the ResourceOwnerPasswordAuthenticator like this
-        var authenticator = new ResourceOwnerPasswordGrantAuthenticator("vaas-customer", UserName, Password, TokenUrl);
-        // You may use self registration and create a new username and password for the
-        // ResourceOwnerPasswordAuthenticator by yourself like the example above on https://vaas.gdata.de/login
-
-        // Else if you got a client id and client secret from us, you can use the ClientCredentialsGrantAuthenticator like this
-        // var authenticator = new ClientCredentialsGrantAuthenticator(
-        //     ClientId,
-        //     ClientSecret,
-        //     TokenUrl
-        // );
-        await vaas.Connect(await authenticator.GetToken());
+            TokenUrl = TokenUrl,
+            Credentials = new TokenRequest
+            {
+                GrantType = GrantType.Password,
+                ClientId = ClientIdForResourceOwnerPasswordGrant,
+                UserName = UserName,
+                Password = Password
+            }
+        });
+     
+        // // Else if you got a client id and client secret from us, you should use the GrantType.ClientCredentials like this
+        // var vaas = VaasFactory.Create(new VaasOptions()
+        // {
+        //     Url = VaasUrl,
+        //     TokenUrl = TokenUrl,
+        //     Credentials = new TokenRequest
+        //     {
+        //         GrantType = GrantType.ClientCredentials,
+        //         ClientId = "client_id",
+        //         ClientSecret = "client_secret"
+        //     }
+        // });
+        await vaas.Connect(CancellationToken.None);
         return vaas;
     }
+    
+    
 
     private static async Task UrlScan()
     {
         var vaas = await CreateVaasAndConnect();
 
         var uri = new Uri("https://secure.eicar.org/eicar.com.txt");
-        var verdict = await vaas.ForUrlAsync(uri);
+        var verdict = await vaas.ForUrlAsync(uri, CancellationToken.None);
 
         Console.WriteLine($"{verdict.Sha256} is detected as {verdict.Verdict}");
     }
