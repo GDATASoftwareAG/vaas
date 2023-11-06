9bec0ee15b75f91240168193ae745be995c4be97
doxthree <kevin.heise@tutanota.de>
2023-11-06 10:42:22 +0100

    Dotnet/http auth (#322)
    * Dotnet: Cache access token
    
    * Dotnet: VaasFactory for use without DI
    
    ---------
    
    Co-authored-by: Philip Stadermann <philip.stadermann@gdata.de>

---
+++ dotnet/Vaas/src/Vaas/VaasFactory.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Net.Http;
+using Vaas.Authentication;
+
+namespace Vaas;
+
+public static class VaasFactory
+{
+    private static readonly HttpClient HttpClient = new ();
+    
+    public static IVaas Create(VaasOptions vaasOptions)
+    {
+        var systemClock = new SystemClock();
+        var authenticator = new Authenticator(HttpClient, systemClock, vaasOptions);
+
+        var bearerTokenHandler = new BearerTokenHandler(authenticator);
+        bearerTokenHandler.InnerHandler = new HttpClientHandler();
+        var httpClient = new HttpClient(bearerTokenHandler);
+
+        return new Vaas(httpClient, authenticator, vaasOptions);
+    }
+}
