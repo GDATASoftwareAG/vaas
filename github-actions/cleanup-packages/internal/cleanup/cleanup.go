c1e3b193f94f7268a1b3fc3e370abbc0617c1c07
unglaublicherdude <github@matthias-simonis.de>
2024-01-22 14:47:29 +0100

    replaces the github action with a go script

---
+++ github-actions/cleanup-packages/internal/cleanup/cleanup.go
@@ -0,0 +1,127 @@
+package cleanup
+
+import (
+	"context"
+	"log"
+	"regexp"
+	"time"
+
+	"github.com/google/go-github/v58/github"
+)
+
+const gdataOrganisation = "GDATASoftwareAG"
+
+var packageType = "container"
+var NameRegex, _ = regexp.Compile(`(gdscanserver|scanclient|vaas/.+)$`)
+var TagsNotToDelete, _ = regexp.Compile(`(latest|[0-9]*\.[0-9]*\.[0-9]*|[0-9]*)$`)
+var nowTime = time.Now()
+
+func Cleanup(client *github.Client) {
+	start := time.Now()
+	ctx := context.Background()
+
+	packageList := getContainerPackages(ctx, client, NameRegex)
+	for _, pack := range packageList {
+		println("checking versions for package ", pack.GetName())
+		versions := getVersionsOlderThan2Month(ctx, client, pack.Name)
+		if len(versions) == 0 {
+			log.Println("no versions older than 2 month for package ", pack.GetName())
+		}
+		for _, version := range versions {
+			client.Organizations.PackageDeleteVersion(ctx, gdataOrganisation, packageType, pack.GetName(), *version.ID)
+			for _, tag := range version.Metadata.Container.Tags {
+				log.Printf("deleted %v:%v \n", pack.GetName(), tag)
+			}
+		}
+	}
+	log.Printf("Deletion took %s", time.Since(start))
+}
+
+func getVersionsOlderThan2Month(context context.Context, client *github.Client, name *string) (versionList []*github.PackageVersion) {
+	run := true
+	nextPage := 1
+	for run {
+		packageVersions, response, err := client.Organizations.PackageGetAllVersions(
+			context, gdataOrganisation, packageType, *name,
+			&github.PackageListOptions{
+				ListOptions: github.ListOptions{
+					Page:    nextPage,
+					PerPage: 10,
+				},
+			})
+		if err != nil {
+			log.Println(response)
+			log.Println(err)
+			run = false
+		}
+		if response != nil {
+			if response.NextPage == 0 {
+				run = false
+			}
+			nextPage = response.NextPage
+		}
+		for _, packVersion := range packageVersions {
+			if isNewestDateOlderThan2Month(&nowTime, &packVersion.CreatedAt.Time, &packVersion.UpdatedAt.Time) {
+				if areWeAllowedToDeleteThisVersion(packVersion.Metadata.Container.Tags) {
+					versionList = append(versionList, packVersion)
+				}
+			}
+		}
+	}
+	return
+}
+
+func areWeAllowedToDeleteThisVersion(tags []string) bool {
+	for _, tag := range tags {
+		if TagsNotToDelete.Match([]byte(tag)) {
+			return false
+		}
+	}
+	return true
+}
+
+func isNewestDateOlderThan2Month(now *time.Time, created *time.Time, update *time.Time) bool {
+	compareTime := update.AddDate(0, 2, 0)
+	if created.After(*update) {
+		compareTime = created.AddDate(0, 2, 0)
+	}
+
+	return compareTime.After(*now)
+}
+
+func getContainerPackages(context context.Context, client *github.Client, nameRegex *regexp.Regexp) (packageList []*github.Package) {
+	run := true
+	nextPage := 1
+	for run {
+		packages, response, err := client.Organizations.ListPackages(
+			context, gdataOrganisation, &github.PackageListOptions{
+				PackageType: &packageType,
+				ListOptions: github.ListOptions{
+					PerPage: 5,
+					Page:    nextPage,
+				},
+			})
+		if err != nil {
+			log.Println(response)
+			log.Println(err)
+			run = false
+		}
+		if response != nil {
+			if response.NextPage == 0 {
+				run = false
+			}
+			nextPage = response.NextPage
+		}
+		for _, pack := range packages {
+			if match(nameRegex, pack.Name) {
+				packageList = append(packageList, pack)
+			}
+		}
+	}
+	return
+}
+
+func match(nameRegex *regexp.Regexp, name *string) bool {
+	nameByteArray := []byte(*name)
+	return nameRegex.Match(nameByteArray)
+}
---
+++ github-actions/cleanup-packages/internal/cleanup/cleanup_test.go
@@ -0,0 +1,103 @@
+package cleanup
+
+import (
+	"testing"
+	"time"
+)
+
+func TestMatch_Inputgdscanserver_ShouldMatches(t *testing.T) {
+	name := "gdscanserver"
+	if !match(NameRegex, &name) {
+		t.Errorf("Name %v should have matched", name)
+	}
+}
+
+func TestMatch_Inputgdscanserver1_ShouldNotMatches(t *testing.T) {
+	name := "gdscanserver1"
+	if match(NameRegex, &name) {
+		t.Errorf("Name %v should not have matched", name)
+	}
+}
+
+func TestMatch_Inputvaas_ShouldNotMatches(t *testing.T) {
+	name := "vaas"
+	if match(NameRegex, &name) {
+		t.Errorf("Name %v should not have matched", name)
+	}
+}
+
+func TestMatch_InputvaaswithSlashAndSomethingbehind_ShouldMatches(t *testing.T) {
+	name := "vaas/server"
+	if !match(NameRegex, &name) {
+		t.Errorf("Name %v should have matched", name)
+	}
+}
+
+func TestMatch_InputvaaswithSlashAndNothingbehind_ShouldNotMatches(t *testing.T) {
+	name := "vaas/"
+	if match(NameRegex, &name) {
+		t.Errorf("Name %v should have matched", name)
+	}
+}
+
+var testNowTime = time.Date(2023, 6, 1, 0, 0, 0, 0, time.UTC)
+
+func TestDateCompare_CreatedAndUpdatedAreTheSameAndOlderThan2Month_ShouldReturnTrue(t *testing.T) {
+	created := time.Date(2023, 4, 1, 0, 0, 0, 0, time.UTC)
+	updated := time.Date(2023, 4, 1, 0, 0, 0, 0, time.UTC)
+	if isNewestDateOlderThan2Month(&testNowTime, &created, &updated) {
+		t.Error("created and updated are older than 2 month so it should be true")
+	}
+}
+
+func TestDateCompare_CreatedAndUpdatedAreTheSameAndNotOlderThan2Month_ShouldReturnFalse(t *testing.T) {
+	created := time.Date(2023, 5, 1, 0, 0, 0, 0, time.UTC)
+	updated := time.Date(2023, 5, 1, 0, 0, 0, 0, time.UTC)
+	if !isNewestDateOlderThan2Month(&testNowTime, &created, &updated) {
+		t.Error("created and updated are not older than 2 month so it should be false")
+	}
+}
+
+func TestDateCompare_UpdatedIsYoungerAndNotOlderThan2Month_ShouldReturnFalse(t *testing.T) {
+	created := time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC)
+	updated := time.Date(2023, 5, 1, 0, 0, 0, 0, time.UTC)
+	if !isNewestDateOlderThan2Month(&testNowTime, &created, &updated) {
+		t.Error("updated is not older than 2 month so it should be false")
+	}
+}
+
+func TestDateCompare_CreatedIsYoungerAndNotOlderThan2Month_ShouldReturnFalse(t *testing.T) {
+	created := time.Date(2023, 5, 1, 0, 0, 0, 0, time.UTC)
+	updated := time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC)
+	if !isNewestDateOlderThan2Month(&testNowTime, &created, &updated) {
+		t.Error("created is not older than 2 month so it should be false")
+	}
+}
+
+func TestAreWeAllowedToDeleteThisVersion_VersionTagWithSingleNumber_NotAllowed(t *testing.T) {
+	tags := []string{"1", "1.1.1-8989"}
+	if areWeAllowedToDeleteThisVersion(tags) {
+		t.Error("this version should not be allowed to delete")
+	}
+}
+
+func TestAreWeAllowedToDeleteThisVersion_VersionTagWithTrippleNumber_NotAllowed(t *testing.T) {
+	tags := []string{"1.1.1", "1.1.1-8989"}
+	if areWeAllowedToDeleteThisVersion(tags) {
+		t.Error("this version should not be allowed to delete")
+	}
+}
+
+func TestAreWeAllowedToDeleteThisVersion_VersionTagWithLatest_NotAllowed(t *testing.T) {
+	tags := []string{"1.1.1-8989", "latest"}
+	if areWeAllowedToDeleteThisVersion(tags) {
+		t.Error("this version should not be allowed to delete")
+	}
+}
+
+func TestAreWeAllowedToDeleteThisVersion_NoException_Allowed(t *testing.T) {
+	tags := []string{"1.1.1-8989", "1-8213"}
+	if areWeAllowedToDeleteThisVersion(tags) {
+		t.Error("this version should not be allowed to delete")
+	}
+}
