38acf2eafe2651ed47c54074b085656659a47a18
Matthias Simonis <github@matthias-simonis.de>
2024-07-03 08:39:29 +0200

    fixes the image cleanup (#501)
    Co-authored-by: Simonis, Matthias <Matthias.Simonis@gdata.de>

--- github-actions/cleanup-packages/internal/cleanup/cleanup.go
+++ github-actions/cleanup-packages/internal/cleanup/cleanup.go
@@ -13,8 +13,8 @@ import (
 const gdataOrganisation = "GDATASoftwareAG"
 
 var packageType = "container"
-var globalNameRegex, _ = regexp.Compile(`(gdscanserver|scanclient|vaas/.+)$`)
-var globalTagsNotToDelete, _ = regexp.Compile(`(latest|[0-9]+\.[0-9]+\.[0-9]+|[0-9]+)$`)
+var globalNameRegex, _ = regexp.Compile(`^(gdscanserver|scanclient|vaas/.+)$`)
+var globalTagsNotToDelete, _ = regexp.Compile(`^(latest|[0-9]+\.[0-9]+\.[0-9]+|[0-9]+\.[0-9]+|[0-9]+)$`)
 var nowTime = time.Now()
 
 // Cleanup the main method of this lib
@@ -24,55 +24,81 @@ func Cleanup(client *github.Client) {
 
 	packageList := getContainerPackages(ctx, client, globalNameRegex)
 	for _, pack := range packageList {
+		if pack.GetName() != "vaas/scanner" {
+			continue
+		}
 		println("checking versions for package ", pack.GetName())
 		versions := getVersionsOlderThan2Month(ctx, client, pack.Name)
-		if len(versions) == 0 {
-			log.Println("no versions older than 2 month for package ", pack.GetName())
-		}
-		for _, version := range versions {
-			log.Printf("deleting package %v with versions %v", pack.Name, strings.Join(version.Metadata.Container.Tags, "|"))
+		deleted := 0
+		for version := range versions {
+			deleted++
+			log.Printf(
+				"deleting package %v with versions %v created %v updates %v",
+				pack.GetName(), strings.Join(version.Metadata.Container.Tags, "|"),
+				version.CreatedAt.Time, version.UpdatedAt.Time)
 			client.Organizations.PackageDeleteVersion(ctx, gdataOrganisation, packageType, pack.GetName(), *version.ID)
+		}
+		if deleted == 0 {
+			log.Println("no versions older than 2 month for package ", pack.GetName())
 		}
 	}
 	log.Printf("Deletion took %s", time.Since(start))
 }
 
-func getVersionsOlderThan2Month(context context.Context, client *github.Client, name *string) (versionList []*github.PackageVersion) {
+func getVersionsOlderThan2Month(context context.Context, client *github.Client, name *string) <-chan *github.PackageVersion {
+	ch := make(chan *github.PackageVersion)
 	run := true
 	nextPage := 1
 	packageType := "container"
-	packageState := "active"
-	for run {
-		packageVersions, response, err := client.Organizations.PackageGetAllVersions(
-			context, gdataOrganisation, packageType, *name,
-			&github.PackageListOptions{
-				ListOptions: github.ListOptions{
-					Page:    nextPage,
-					PerPage: 100,
-				},
-				PackageType: &packageType,
-				State:       &packageState,
-			})
-		if err != nil {
-			log.Println(response)
-			log.Println(err)
-			run = false
-		}
-		if response != nil {
-			if response.NextPage == 0 {
+	_, lengthResponse, err := client.Organizations.PackageGetAllVersions(
+		context, gdataOrganisation, packageType, *name,
+		&github.PackageListOptions{
+			ListOptions: github.ListOptions{
+				Page:    nextPage,
+				PerPage: 100,
+			},
+			PackageType: &packageType,
+		})
+	if err != nil {
+		log.Println(lengthResponse)
+		log.Println(err)
+		run = false
+	}
+	nextPage = lengthResponse.LastPage
+
+	go func() {
+		defer close(ch)
+		for run {
+			packageVersions, response, err := client.Organizations.PackageGetAllVersions(
+				context, gdataOrganisation, packageType, *name,
+				&github.PackageListOptions{
+					ListOptions: github.ListOptions{
+						Page:    nextPage,
+						PerPage: 100,
+					},
+					PackageType: &packageType,
+				})
+			if err != nil {
+				log.Println(response)
+				log.Println(err)
 				run = false
 			}
-			nextPage = response.NextPage
-		}
-		for _, packVersion := range packageVersions {
-			if isNewestDateOlderThan2Month(&nowTime, &packVersion.CreatedAt.Time, &packVersion.UpdatedAt.Time) {
-				if areWeAllowedToDeleteThisVersion(packVersion.Metadata.Container.Tags) {
-					versionList = append(versionList, packVersion)
+			if response != nil {
+				if response.PrevPage == 0 {
+					run = false
+				}
+				nextPage = response.PrevPage
+			}
+			for _, packVersion := range packageVersions {
+				if isNewestDateOlderThan2Month(&nowTime, &packVersion.CreatedAt.Time, &packVersion.UpdatedAt.Time) {
+					if areWeAllowedToDeleteThisVersion(packVersion.Metadata.Container.Tags) {
+						ch <- packVersion
+					}
 				}
 			}
 		}
-	}
-	return
+	}()
+	return ch
 }
 
 func areWeAllowedToDeleteThisVersion(tags []string) bool {
@@ -87,18 +113,18 @@ func areWeAllowedToDeleteThisVersion(tags []string) bool {
 	return true
 }
 
-func isNewestDateOlderThan2Month(now *time.Time, created *time.Time, update *time.Time) bool {
-	compareTime := update.AddDate(0, 2, 0)
-	if created.After(*update) {
+func isNewestDateOlderThan2Month(now *time.Time, created *time.Time, updated *time.Time) bool {
+	compareTime := updated.AddDate(0, 2, 0)
+	if created.After(*updated) {
 		compareTime = created.AddDate(0, 2, 0)
 	}
 
-	return compareTime.After(*now)
+	return compareTime.Before(*now)
 }
 
 func getContainerPackages(context context.Context, client *github.Client, nameRegex *regexp.Regexp) (packageList []*github.Package) {
 	run := true
-	nextPage := 1
+	nextPage := 0
 	for run {
 		packages, response, err := client.Organizations.ListPackages(
 			context, gdataOrganisation, &github.PackageListOptions{
