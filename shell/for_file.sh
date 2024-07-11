3e6d2a2a08fc6b49c44139772bd1e80f112b5b8d
Philip Stadermann <philip.stadermann@gdata.de>
2024-07-11 13:19:12 +0200

    Shell example #509

---
+++ shell/for_file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source vaas.sh
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 [FILE]..."
+    exit 1
+fi
+
+VAAS_URL="https://upload.staging.vaas.gdatasecurity.de"
+TOKEN_URL="https://account-staging.gdata.de/realms/vaas-staging/protocol/openid-connect/token"
+
+for file in "$@"; do
+    for_file $VAAS_URL $TOKEN_URL $USER_NAME $PASSWORD $file
+done
