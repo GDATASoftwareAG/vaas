d24353a922e6f9140618af321b214fb705a71815
Kevin Heise <kevin.heise@gdata.de>
2024-09-16 08:03:56 +0000

    close after test

--- typescript/tests/test.ts
+++ typescript/tests/test.ts
@@ -68,7 +68,7 @@ async function createVaasWithResourceOwnerPasswordGrantAuthenticator() {
   return vaas;
 }
 
-const defaultTimeout: number = 10_000;
+const defaultTimeout: number = 50_000;
 
 const eicarSha256 =
   "275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f";
@@ -389,6 +389,10 @@ describe("Vaas", () => {
         await expect((vaas as any)[method](...params)).rejects.toThrow(VaasAuthenticationError);
         vaas.close();
       });
+
+      afterAll(() => {
+        vaas.close(); // Beispiel für das Schließen einer Datenbankverbindung
+      });      
     });
   }); 
 });
