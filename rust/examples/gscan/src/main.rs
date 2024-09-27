418e095b584bbade327d9bd5418f9672f1176bd4
Philip Stadermann <philip.stadermann@gdata.de>
2024-09-27 17:27:45 +0200

    Remove unnecessary clone

--- rust/examples/gscan/src/main.rs
+++ rust/examples/gscan/src/main.rs
@@ -126,7 +126,7 @@ fn expand_entry(p: &PathBuf) -> Box<dyn Iterator<Item = PathBuf>> {
         .into_iter()
         .filter_map(|e| e.ok())
         .filter(|e| e.file_type().is_file())
-        .map(|e| e.path().to_path_buf().clone())
+        .map(|e| e.path().to_path_buf())
         .into_iter();
 
     Box::new(files_in_directory)
