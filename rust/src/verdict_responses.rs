6a190d7bc5887361daab0ce78916975ae7e1a93e
Fröhling, Maximilian <Maximilian.Froehling@gdata.de>
2024-10-18 13:45:18 +0200

    SQUASH: Add verdict_responses test case

--- rust/src/verdict_responses.rs
+++ rust/src/verdict_responses.rs
@@ -1,33 +1,57 @@
+use crate::error::VResult;
+use futures::channel::oneshot;
+use futures_util::{TryFutureExt};
 use std::collections::HashMap;
+use std::future::Future;
+use std::sync::Mutex;
 
-use futures::{channel::oneshot, lock::Mutex};
-
-use crate::{error::VResult, message::VerdictResponse};
-
-struct VerdictResponses {
-    responses: Mutex<HashMap<String, oneshot::Sender<VResult<VerdictResponse>>>>,
+struct VerdictResponses<T> {
+    responses: Mutex<HashMap<String, oneshot::Sender<T>>>,
 }
 
-impl VerdictResponses {
+impl<T> VerdictResponses<T> {
     pub fn new() -> Self {
         Self {
             responses: Mutex::new(HashMap::new()),
         }
     }
 
-    pub async fn get_response(&self, request_id: &str) -> VResult<VerdictResponse> {
-        let mut requests = self.responses.lock().await;
-        let (sender, receiver) = oneshot::channel();
-        requests.insert(request_id.to_string(), sender);
-        receiver.await.expect("Sender has been dropped")
+    pub fn get_response(&self, request_id: &str) -> impl Future<Output = VResult<T>> {
+        let receiver = {
+            let mut requests = self.responses.lock().unwrap_or_else(|e| e.into_inner());
+            let (sender, receiver) = oneshot::channel();
+            requests.insert(request_id.to_string(), sender);
+            receiver
+        };
+        receiver.map_err(|_| crate::error::Error::Cancelled)
     }
 
-    pub async fn set_response(&self, request_id: &str, response: VResult<VerdictResponse>) {
-        let mut requests = self.responses.lock().await;
+    pub fn set_response(&self, request_id: &str, response: T) {
+        let mut requests = self.responses.lock().unwrap_or_else(|e| e.into_inner());
         if let Some(r) = requests.remove(request_id) {
-            r.send(response);
+            if r.send(response).is_err() {
+                // TODO: proper logging
+                eprintln!("cannot send");
+            }
+        } else {
+            eprintln!("cannot find request id");
         }
     }
 
     //   set_all_responses
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::verdict_responses::VerdictResponses;
+
+    const TEST_REQUEST_ID: &str = "1234";
+
+    #[tokio::test]
+    pub async fn get_response_returns_verdict_response() {
+        let responses = VerdictResponses::new();
+        let response_future = responses.get_response(TEST_REQUEST_ID);
+        responses.set_response(TEST_REQUEST_ID, 42);
+        assert_eq!(response_future.await.unwrap(), 42);
+    }
 }
