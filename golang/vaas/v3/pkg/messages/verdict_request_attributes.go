003a8023e75b3d4d031d03bbb437dabe446cc6c4
Fröhling, Maximilian <Maximilian.Froehling@gdata.de>
2024-11-22 16:52:21 +0100

    Remove unused websocket messages

--- golang/vaas/v3/pkg/messages/verdict_request_attributes.go
+++
@@ -1,7 +0,0 @@
-// Package messages provides structures for handling communication messages between the client and the VaaS server.
-package messages
-
-// VerdictRequestAttributes represents attributes associated with a verdict request.
-type VerdictRequestAttributes struct {
-	TenantID string `json:"tenantId"`
-}
