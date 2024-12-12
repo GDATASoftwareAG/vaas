d76339401dfc087e686ff6224c48a86aa89574c1
Lennart Dohmann <lennart.dohmann@gdata.de>
2024-12-12 17:32:37 +0100

    WIP Fix examples for real for real

--- php/examples/VaasExample/GetVerdictByHash.php
+++ php/examples/VaasExample/GetVerdictByHash.php
@@ -21,7 +21,7 @@ $vaas = new Vaas($authenticator);
 
 // EICAR
 $vaasVerdict = $vaas->forSha256Async("000005c43196142f01d615a67b7da8a53cb0172f8e9317a2ec9a0a39a1da6fe8")->await();
-fwrite(STDOUT, "Verdict for $vaasVerdict->sha256 is " . $vaasVerdict->verdict . " \n");
+fwrite(STDOUT, "Verdict for $vaasVerdict->sha256 is " . $vaasVerdict->verdict->value . " \n");
 // SOMEFILE
 $vaasVerdict = $vaas->forSha256Async("70caea443deb0d0a890468f9ac0a9b1187676ba3e66eb60a722b187107eb1ea8")->await();
-fwrite(STDOUT, "Verdict for $vaasVerdict->sha256 is " . $vaasVerdict->verdict . " \n");
+fwrite(STDOUT, "Verdict for $vaasVerdict->sha256 is " . $vaasVerdict->verdict->value . " \n");
