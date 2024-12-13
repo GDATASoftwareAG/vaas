213a3edc615ff4b12e15923744979ba763e697b3
Lennart Dohmann <lennart.dohmann@gdata.de>
2024-12-13 16:35:27 +0100

    WIP Fix streaming by adding content-length and add transfer timeout for stream upload

--- php/src/vaas/Options/VaasOptions.php
+++ php/src/vaas/Options/VaasOptions.php
@@ -8,16 +8,19 @@ class VaasOptions
     public ?bool $useCache;
     public string $url;
     public string $tokenUrl;
+    public ?int $timeout;
 
     public function __construct(
         ?bool $useHashLookup = null,
         ?bool $useCache = null,
         string $url = 'https://gateway.production.vaas.gdatasecurity.de/',
-        string $tokenUrl = 'https://account.gdata.de/realms/vaas-production/protocol/openid-connect/token'
+        string $tokenUrl = 'https://account.gdata.de/realms/vaas-production/protocol/openid-connect/token',
+        ?int $timeout = null
     ) {
         $this->url = $url;
         $this->useHashLookup = $useHashLookup;
         $this->useCache = $useCache;
         $this->tokenUrl = $tokenUrl;
+        $this->timeout = $timeout;
     }
 }
