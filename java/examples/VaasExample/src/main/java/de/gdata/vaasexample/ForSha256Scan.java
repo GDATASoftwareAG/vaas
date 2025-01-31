6f698aa8de40d49098f7e9e60be5138a4b3d5e8d
Kevin Heise <kevin.heise@gdata.de>
2025-01-31 10:00:14 +0100

    use only oen authenticator & add line breaks

--- java/examples/VaasExample/src/main/java/de/gdata/vaasexample/ForSha256Scan.java
+++ java/examples/VaasExample/src/main/java/de/gdata/vaasexample/ForSha256Scan.java
@@ -6,22 +6,17 @@ import java.net.URI;
 
 public class ForSha256Scan {
     public static void main(String[] args) throws Exception {
-        IAuthenticator authenticator;
         var env = new Environment();
-        if (env.clientId == null) {
-            if (env.userName == null) {
-                throw new IllegalStateException("Either CLIENT_ID or VAAS_USER_NAME must be set");
-            }
-            authenticator = new ResourceOwnerPasswordGrantAuthenticator("vaas-customer", env.userName, env.password, new URI(env.tokenUrl));
-        } else {
-            authenticator = new ClientCredentialsGrantAuthenticator(env.clientId, env.clientSecret, new URI(env.tokenUrl));
-        }
 
+        var authenticator = new ClientCredentialsGrantAuthenticator(env.clientId, env.clientSecret, new URI(env.tokenUrl));
         var config = new VaasConfig(new URI(env.vaasUrl));
         var vaas = new Vaas(config, authenticator);
+
         var sha256 = new Sha256("275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f");
         var verdict = vaas.forSha256(sha256);
+
         System.out.printf("File %s was sync detected as %s", verdict.getSha256(), verdict.getVerdict());
+
         vaas.forSha256Async(sha256).thenAccept(vaasResult -> {
             System.out.printf("\nFile %s was async detected as %s", vaasResult.getSha256(), vaasResult.getVerdict());
         }).get();
--- java/examples/VaasExample/src/main/java/de/gdata/vaasexample/ForStreamScan.java
+++ java/examples/VaasExample/src/main/java/de/gdata/vaasexample/ForStreamScan.java
@@ -9,23 +9,17 @@ import java.nio.file.Path;
 
 public class ForStreamScan {
     public static void main(String[] args) throws Exception {
-        IAuthenticator authenticator;
         var env = new Environment();
-        if (env.clientId == null) {
-            if (env.userName == null) {
-                throw new IllegalStateException("Either CLIENT_ID or VAAS_USER_NAME must be set");
-            }
-            authenticator = new ResourceOwnerPasswordGrantAuthenticator("vaas-customer", env.userName, env.password, new URI(env.tokenUrl));
-        } else {
-            authenticator = new ClientCredentialsGrantAuthenticator(env.clientId, env.clientSecret, new URI(env.tokenUrl));
-        }
 
+        var authenticator = new ClientCredentialsGrantAuthenticator(env.clientId, env.clientSecret, new URI(env.tokenUrl));
         var config = new VaasConfig(new URI(env.vaasUrl));
         var vaas = new Vaas(config, authenticator);
+
         try (var inputStream = Files.newInputStream(Path.of(Environment.getenv("SCAN_PATH")))) {
             var verdict = vaas.forStream(inputStream, Path.of(Environment.getenv("SCAN_PATH")).toFile().length());
             System.out.printf("File %s was sync detected as %s", verdict.getSha256(), verdict.getVerdict());
         }
+
         try (var inputStream = Files.newInputStream(Path.of(Environment.getenv("SCAN_PATH")))) {
             vaas.forStreamAsync(inputStream, Path.of(Environment.getenv("SCAN_PATH")).toFile().length()).thenAccept(vaasResult -> {
                 System.out.printf("\nFile %s was async detected as %s", vaasResult.getSha256(), vaasResult.getVerdict());
