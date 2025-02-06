1cc5740f0a4beaf14abcad6294839967f777368e
Lennart Dohmann <mail@dohmann.cloud>
2025-02-06 07:53:25 +0100

    Add detailed exception documentation for getTokenAsync method

--- php/src/vaas/Authentication/TokenReceiver.php
+++ php/src/vaas/Authentication/TokenReceiver.php
@@ -36,6 +36,7 @@ abstract class TokenReceiver
      * If the token is expired, a new token will be requested.
      * @param Cancellation|null $cancellation Cancellation token
      * @return Future Future that resolves to the access token as string
+     * @throws VaasAuthenticationException The Vaas authentication failed. Recommended actions: Double-check your credentials in the authenticator object. Check if your authenticator connects to the correct token endpoint. Check if the token endpoint is reachable. If your problem persists contact G DATA.
      */
     public function getTokenAsync(?Cancellation $cancellation = null): Future
     {
